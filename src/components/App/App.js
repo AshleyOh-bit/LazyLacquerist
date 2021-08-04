7f09956ccb62bc6283e7420325c3e5c7cbf3022c
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-08-04 15:56:00 -0600

    Build App structure with api call functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,35 @@
-// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          hello buddy
-        </p>
+import { apiCall } from "../../utilities/apiCalls"
+
+import nails from "../../assets/mani-icon.png"
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state ={
+      polishes: []
+    }
+  }
+
+  componentDidMount() {
+    apiCall("nail_polish")
+    .then(response => console.log(response))
+  }
+
+  render() {
+    return (
+    <main>
+      <header>
+        <h1 className="title">The Lazy Lacquerist</h1>
       </header>
-    </div>
-  );
+      <section>
+        <img className="nails" src={nails}/>
+      </section>
+    </main>
+    )
+  }
 }
 
 export default App;
