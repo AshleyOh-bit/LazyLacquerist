c917f67a3fc9bb9cea017d798dc5d09adc913aa8
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-08-07 18:23:50 -0600

    Add new background to landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "./Home.css"
 import { Link } from "react-router-dom"
+import bottle from "../../assets/bottle-icon.png"
 
 export const Home = props => {
   return (
@@ -9,8 +10,8 @@ export const Home = props => {
         <Link to="/mani-maker"> <button className="nav-home">Mani-Maker</button> </Link>
         <Link to="/collection"> <button className="nav-home">My Collection</button> </Link>
       </section>
-      <section className="polish-bottle">
-        <img src="../../bottle-icon.jpeg" />
+      <section className="bottle-section">
+        <img className="bottle-icon" src={bottle} alt={"An illustration of a nail polish bottle with a teal background"}/>
       </section>
     </section>
   )
