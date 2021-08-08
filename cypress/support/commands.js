556f75e9256290f09fdbb30e47d57175d7529668
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-08-08 12:24:11 -0600

    Add home view tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,6 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("loadMain", () => {
+  const baseURL = "http://makeup-api.herokuapp.com/api/v1/products.json?product_type="
+  cy.intercept('GET', `${baseURL}nail_polish`, 
+      { statusCode: 200, fixture: 'polishes.json' })
+  cy.visit(`http://localhost:3000`)
+})
