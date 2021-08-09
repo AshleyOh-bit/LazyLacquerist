895ebf753a7776c2a3413b60ab5ec2c36f312677
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-08-09 12:28:31 -0600

    Style home page to be responsive across screen sizes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,8 +6,10 @@ import bottle from "../../assets/bottle-icon.png"
 export const Home = () => {
   return (
     <section className="home-view">
-      <h2>Welcome!</h2> 
-      <h3>Would you like to generate a new mani palette or see your collection?</h3>
+      <section className="greeting">
+        <h2>Welcome!</h2> 
+        <h3>Would you like to generate a new mani palette or see your collection?</h3>
+      </section>
       <section className="buttons">
         <Link to="/mani-maker"> <button data-cy="maniMaker" className="nav-home">Mani-Maker</button> </Link>
         <Link to="/collection"> <button data-cy="myColl" className="nav-home">My Collection</button> </Link>
