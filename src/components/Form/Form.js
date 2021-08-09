666e8851fab8fb259a9f6b523d8df240eedb43d7
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-08-09 14:41:17 -0600

    Customize circle color picker colors

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -145,6 +145,7 @@ export class Form extends React.Component {
               disabled={this.state.inputStatus}
               color={this.state.hue} 
               onChange={this.setHue}
+              colors={["#f44336", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#03a9f4", "#00bcd4", "#009688", "#4caf50", "#8bc34a", "#cddc39", "#ffeb3b", "#ffc107", "beige", "lightgrey", "grey", "black"]}
             />
           </section>
           <Link to="/collection"><button 
